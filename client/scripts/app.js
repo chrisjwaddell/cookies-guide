1486a5be78f0093afbf4251e87e41d66966c8242
Chris Waddell <yesitschris@hotmail.com>
2023-01-14 22:46:24 +1100

    All attributes sections finished

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,46 +4,96 @@ const showCookieValue = () => elCookieValue.textContent = document.cookie.split(
 
 
 const TOC = [
-{
-	name: 'Basic Cookie 1',
-	filename: '01-basic-cookie-1.html',
-},
-{
-	name: 'Basic Cookie 2',
-	filename: '02-basic-cookie-2.html',
-},
-{
-	name: 'Uses of Cookies',
-	filename: '03-uses.html',
-},
-{
-	name: 'Reading Cookies',
-	filename: '04-reading-cookies.html',
-},
-{
-	name: 'The Cookie header and requests',
-	filename: '05-the-cookie-header.html',
-},
-{
-	name: 'Sending cookies to a server',
-	filename: '06-send-cookie-to-server.html',
-},
-{
-	name: 'Attributes - expires and max-age',
-	filename: '07-cookie-expiry.html',
-},
-{
-	name: 'Attributes - domain',
-	filename: '08-cookie-domain.html',
-},
-{
-	name: 'Attributes - path',
-	filename: '09-path.html',
-},
-{
-	name: 'Basic Cookie 1',
-	filename: '01-',
-}]
+	{
+		name: 'Basic Cookie 1',
+		filename: '01-basic-cookie-1.html',
+	},
+	{
+		name: 'Basic Cookie 2',
+		filename: '02-basic-cookie-2.html',
+	},
+	{
+		name: 'Uses of Cookies',
+		filename: '03-uses.html',
+	},
+	{
+		name: 'Reading Cookies',
+		filename: '04-reading-cookies.html',
+	},
+	{
+		name: 'The Cookie header and requests',
+		filename: '05-the-cookie-header.html',
+	},
+	{
+		name: 'Sending cookies to a server',
+		filename: '06-send-cookie-to-server.html',
+	},
+	{
+		name: 'Attributes - expires and max-age',
+		filename: '07-cookie-expiry.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - path',
+		filename: '09-path.html',
+	},
+	{
+		name: 'Attributes - httpOnly',
+		filename: '10-httponly.html',
+	},
+	{
+		name: 'Attributes - samesite',
+		filename: '11-samesite.html',
+	},
+	{
+		name: 'Attributes - secure',
+		filename: '12-secure.html',
+	},
+	{
+		name: 'Some code funcions for cookies',
+		filename: '13-code.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+	{
+		name: 'Attributes - domain',
+		filename: '08-cookie-domain.html',
+	},
+
+]
 
 
 /* ******************************************************************************
--- server/routes/cookies.js
+++ server/routes/cookies.js
@@ -26,4 +26,147 @@ router.get('/sendcookie', (req, res) => {
 })
 
 
+
+router.get('/client/sendcookie', (req, res) => {
+	console.log(req.headers.cookie)
+	// console.log(req.cookie)
+
+	// res.set('Set-Cookie', 'from=server');
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+	// res.status(200).send(`Cookie received on the server. Cook value was ${req.cookie}`)
+	res.send({
+		message: `Cookie received on the server. Cookie value was ${req.headers.cookie}`,
+	})
+	// res.send('Cookie received on the server.')
+})
+
+
+// I don't think you can send multiple cookies in 'Set-Cookie' so I'm doing 4 requests in one button click.
+router.get('/receivecookies1', (req, res) => {
+	console.log('receivecookies')
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+
+	res.set('Set-Cookie', 'from_server_root=root; path=/;')
+	// res.set('Set-Cookie', 'from_server_client=client; path=/client')
+	// res.set('Set-Cookie', 'from_server_subdir1=subdir1; path=/client/subdir1;')
+	// res.set('Set-Cookie', 'from_server_subdir2=subdir2 path=/client/subdir2;')
+
+
+	res.send({
+		message: 'Cookie 1 sent back to the browser.',
+	})
+})
+
+router.get('/receivecookies2', (req, res) => {
+	console.log('receivecookies')
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+
+	// res.set('Set-Cookie', 'from_server_root=root; path=/;')
+	res.set('Set-Cookie', 'from_server_client=client; path=/client')
+	// res.set('Set-Cookie', 'from_server_subdir1=subdir1; path=/client/subdir1;')
+	// res.set('Set-Cookie', 'from_server_subdir2=subdir2 path=/client/subdir2;')
+
+
+	res.send({
+		message: 'Cookie 2 sent back to the browser.',
+	})
+})
+
+router.get('/receivecookies3', (req, res) => {
+	console.log('receivecookies')
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+
+	// res.set('Set-Cookie', 'from_server_root=root; path=/;')
+	// res.set('Set-Cookie', 'from_server_client=client; path=/client')
+	res.set('Set-Cookie', 'from_server_subdir1=subdir1; path=/client/subdir1;')
+	// res.set('Set-Cookie', 'from_server_subdir2=subdir2 path=/client/subdir2;')
+
+
+	res.send({
+		message: 'Cookie 3 sent back to the browser.',
+	})
+})
+
+
+router.get('/receivecookies4', (req, res) => {
+	console.log('receivecookies')
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+
+	// res.set('Set-Cookie', 'from_server_root=root; path=/;')
+	// res.set('Set-Cookie', 'from_server_client=client; path=/client')
+	// res.set('Set-Cookie', 'from_server_subdir1=subdir1; path=/client/subdir1;')
+	res.set('Set-Cookie', 'from_server_subdir2=subdir2; path=/client/subdir2;')
+
+
+	res.send({
+		message: 'Cookie 4 sent back to the browser.',
+	})
+})
+
+
+
+
+router.get('/httponly', (req, res) => {
+	console.log('httponly')
+
+	res.set('Access-Control-Allow-Origin', req.headers.origin); // req.headers.origin
+	res.set('Access-Control-Allow-Credentials', 'true');
+	// access-control-expose-headers allows JS in the browser to see headers other than the default 7
+	res.set(
+		'Access-Control-Expose-Headers',
+		'date, etag, access-control-allow-origin, access-control-allow-credentials',
+	);
+
+
+	// res.set('Set-Cookie', 'from_server_root=root; path=/;')
+	// res.set('Set-Cookie', 'from_server_client=client; path=/client')
+	// res.set('Set-Cookie', 'from_server_subdir1=subdir1; path=/client/subdir1;')
+	res.set('Set-Cookie', 'httponly_cookie=httponly; httpOnly;')
+
+	res.send({
+		message: 'httponly Cookie sent back to the browser.',
+	})
+})
+
+
 module.exports = router;
